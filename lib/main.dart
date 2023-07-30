89d6de15d9a04389b457e28377bb9c97c2bf87b7
shixin <shixin1359@gmail.com>
2023-07-30 10:41:11 +0800

    GridView实现网格布局

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 
-const cityNames = {
-  '北京': ['东城区', '西城区', '朝阳区', '丰台区', '石景山区', '海淀区', '顺义区'],
-  '上海': ['黄浦区', '徐汇区', '长宁区', '静安区', '普陀区', '闸北区', '虹口区'],
-  '广州': ['越秀', '海珠', '荔湾', '天河', '白云', '黄埔', '南沙', '番禺'],
-  '深圳': ['南山', '福田', '罗湖', '盐田', '龙岗', '宝安', '龙华'],
-  '杭州': ['上城区', '下城区', '江干区', '拱墅区', '西湖区', '滨江区'],
-  '苏州': ['姑苏区', '吴中区', '相城区', '高新区', '虎丘区', '工业园区', '吴江区']
-};
+const cityNames = ['北京', '上海', '广州', '深圳', '重庆', '武汉', '杭州', '西安', '郑州', '南京', '厦门', '大连'];
 
 void main() {
   runApp(const MyApp());
@@ -21,48 +14,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String countString = '';
-  String localCount = '';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('ExpansionTile实现可展开的列表',),
+          title: const Text('网格布局',),
         ),
-        body: ListView(
+        body: GridView.count(
+          crossAxisCount: 2,
           children: _buildList(),
-        ),
+        )
       ),
     );
   }
 
   List<Widget> _buildList() {
-    List<Widget> widgets = [];
-    for (var city in cityNames.keys) {
-      widgets.add(_item(city, cityNames[city]!));
-    }
-    return widgets;
+    return cityNames.map((city) => _item(city)).toList();
   }
 
-  Widget _item(String city, List<String> subCities) {
-    return ExpansionTile(
-      title: Text(city,style: const TextStyle(color: Colors.black54,fontSize: 20),),
-      children: subCities.map((subCity) => _buildSub(subCity)).toList(),
-    );
-  }
-
-  Widget _buildSub(String subCity) {
-    return FractionallySizedBox(
-      widthFactor: 1.0,
-      child: Container(
-        height: 50,
-        margin: const EdgeInsets.only(bottom: 5),
-        decoration: const BoxDecoration(color: Colors.teal),
-        alignment: Alignment.center,
-        child: Text(subCity),
-      ),
+  Widget _item(String city) {
+    return Container(
+      height: 80,
+      margin: const EdgeInsets.only(bottom: 5,right: 5),
+      decoration: const BoxDecoration(color: Colors.teal),
+      alignment: Alignment.center,
+      child: Text(city,style: const TextStyle(color: Colors.white,fontSize: 20),),
     );
   }
 }
